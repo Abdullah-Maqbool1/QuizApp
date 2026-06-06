16839c66486d21de1584657c3cd3ed36251e8855
Abdullah Maqbool - Coding Enthusiast <maqboolmianabdullah@gmail.com>
2026-06-06 19:15:37 +0500

    removed some unused code from ui folder

--- src/main/java/com/quizapp/ui/ResultsPanel.java
+++ src/main/java/com/quizapp/ui/ResultsPanel.java
@@ -15,36 +15,36 @@ public class ResultsPanel extends JPanel {
     private final JLabel messageLabel = new JLabel("", SwingConstants.CENTER);
 
     public ResultsPanel(QuizFrame frame) {
-        setLayout(new GridBagLayout());
-
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.insets = new Insets(10, 10, 10, 10);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
 
         JLabel title = new JLabel("Results");
         title.setFont(new Font("SansSerif", Font.BOLD, 36));
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         scoreLabel.setFont(new Font("SansSerif", Font.BOLD, 24));
+        scoreLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         percentLabel.setFont(new Font("SansSerif", Font.PLAIN, 20));
+        percentLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         messageLabel.setFont(new Font("SansSerif", Font.ITALIC, 16));
+        messageLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         JButton home = new JButton("Back to Home");
         home.setFont(new Font("SansSerif", Font.BOLD, 16));
+        home.setAlignmentX(Component.CENTER_ALIGNMENT);
         home.addActionListener(e -> frame.show(QuizFrame.HOME));
 
-        gbc.gridy = 0;
-        add(title, gbc);
-        gbc.gridy = 1;
-        add(scoreLabel, gbc);
-        gbc.gridy = 2;
-        add(percentLabel, gbc);
-        gbc.gridy = 3;
-        add(messageLabel, gbc);
-        gbc.gridy = 4;
-        gbc.insets = new Insets(28, 10, 10, 10);
-        add(home, gbc);
+        add(title);
+        add(Box.createRigidArea(new Dimension(0, 12)));
+        add(scoreLabel);
+        add(Box.createRigidArea(new Dimension(0, 8)));
+        add(percentLabel);
+        add(Box.createRigidArea(new Dimension(0, 8)));
+        add(messageLabel);
+        add(Box.createRigidArea(new Dimension(0, 28)));
+        add(home);
     }
 
     /** Pulls final numbers from the ScoreManager singleton. */
